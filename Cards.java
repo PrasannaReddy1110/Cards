87828ab4ce79ce0636e3d4a54086bebd4f37899c
Nitya sri <Nitya.Nellore@cdk.com>
2016-07-08 09:02:29 +0530

    added sorted hand method.

--- Cards.java
+++ Cards.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Compare {
 	
 	int[] value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
@@ -9,6 +10,8 @@ class Compare {
 	public int getPip(int sequence) {
 		return sequence % 13;
 	}
+
+
 	public int compare(int seq1, int seq2){
 		if( getPip(seq1) > getPip(seq2) )
 			return seq1;
@@ -25,14 +28,23 @@ class Compare {
 	
 	
 	}
+   	 public ArrayList<Integer> sortedHand( ArrayList<Integer> hand) {
+    		Collections.sort(hand);
+      	 return hand;
+    }
 }
 public class Cards {
 	public static void main(String[] args) {
 		int seq1 = 43;
 		int seq2 = 47;
+		ArrayList<Integer> hand = new ArrayList<Integer>();
+		hand.add(5);
+		hand.add(9);
+		hand.add(10);
 		Compare c = new Compare();
 		System.out.println(c.getSuit(seq1));
 		System.out.println(c.getPip(seq2));
 		System.out.println(c.compare(seq1,seq2));
+		System.out.println(c.sortedHand(hand));
 	}
 }
