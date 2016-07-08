c9a9dbd5682f4d6f571bab2d5f2d8152615a42db
Nitya sri <Nitya.Nellore@cdk.com>
2016-07-08 10:02:35 +0530

    added player class

--- Cards.java
+++ Cards.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Compare {
+class Card {
 	
 	int[] value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 	String[] suit = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
@@ -13,6 +13,7 @@ class Compare {
 
 
 	public int compare(int seq1, int seq2){
+		int EqualCards = -1;
 		if( getPip(seq1) > getPip(seq2) )
 			return seq1;
 		else if( getPip(seq1) < getPip(seq2))
@@ -23,20 +24,25 @@ class Compare {
 			else if( getSuit(seq1) < getSuit(seq2))
 				return seq2;
 			else
-				return -1;
-		}	
-	
-	
-	}
-   	 public ArrayList<Integer> sortedHand( ArrayList<Integer> hand) {
-    		Collections.sort(hand);
-      	 return hand;
-    	 }
-	 public void shuffle(ArrayList<Cards> cards){
+				return EqualCards;
+		}
+	}	
+	public void shuffle(ArrayList<Integer> cards){
     	
      		Collections.shuffle(cards);
     	 }
 }
+ class Player {
+ 	 public void pickCard(){
+ 	 }
+ 	 public void dropCard(int i){
+ 	 }
+ 	 public ArrayList<Integer> sortedHand( ArrayList<Integer> hand) {
+ 	         Collections.sort(hand);
+ 	   return hand;
+ 	 }
+}
+
 public class Cards {
 	public static void main(String[] args) {
 		int seq1 = 43;
@@ -46,10 +52,11 @@ public class Cards {
 		hand.add(9);
 		hand.add(10);
 		hand.add(20);
-		Compare c = new Compare();
+		Card c = new Card();
+		Player p  = new Player();
 		System.out.println(c.getSuit(seq1));
 		System.out.println(c.getPip(seq2));
 		System.out.println(c.compare(seq1,seq2));
-		System.out.println(c.sortedHand(hand));
+		System.out.println(p.sortedHand(hand));
 	}
 }
